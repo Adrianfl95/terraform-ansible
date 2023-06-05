cb7a2717df1927b66f50c5464c04487f8eace618
Adrianfl95 <119310384+Adrianfl95@users.noreply.github.com>
2023-06-05 11:35:32 +0200

    Create variables.tf

---
+++ terraform/vpc/variables.tf
@@ -0,0 +1,12 @@
+# vpc/variables.tf
+
+variable "vpc_name" {
+  description = "Nombre de la VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR de la VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
