adfa03fbf43923f45b136b1bf3b31570a8402e9b
Adrianfl95 <119310384+Adrianfl95@users.noreply.github.com>
2023-06-05 11:30:41 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,25 @@
 # main.tf
+
+terraform {
+  required_version = ">= 1.0"
+}
+
 provider "google" {
   credentials = file("<RUTA_AL_ARCHIVO_DE_CREDENCIALES>")
   project     = "<ID_DEL_PROYECTO>"
   region      = "us-central1"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-vpc-network"
-  auto_create_subnetworks = false
+module "vpc" {
+  source    = "./vpc"
+  vpc_name  = "my-vpc"
+  vpc_cidr  = "10.0.0.0/16"
 }
 
-resource "google_compute_subnetwork" "vpc_subnetwork" {
-  name          = "my-vpc-subnetwork"
-  ip_cidr_range = "10.0.0.0/24"
-  network       = google_compute_network.vpc_network.name
-}
-
-resource "google_compute_address" "static_ip" {
-  name = "my-static-ip"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "my-vm-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
-    }
-  }
-
-  network_interface {
-    subnetwork         = google_compute_subnetwork.vpc_subnetwork.self_link
-    access_config {
-      nat_ip = google_compute_address.static_ip.address
-    }
-  }
+module "vm" {
+  source           = "./vm"
+  vm_name          = "my-vm"
+  vm_machine_type  = "e2-micro"
+  vm_zone          = "us-central1-a"
+  vpc_network_name = module.vpc.vpc_network_name
 }
